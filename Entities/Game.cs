997da4c46bcc367ad5ba5330d534c47c4ef27c60
suresh <surace7523@gmail.com>
2024-06-09 14:45:46 -0400

    first commit

---
+++ Entities/Game.cs
@@ -0,0 +1,23 @@
+namespace Gamestore.Api.Entities;
+
+public class Game
+{
+    public int Id { get; set; }
+
+
+    public required string Name { get; set; }
+
+
+    public required string Genre { get; set; }
+
+
+    public decimal Price { get; set; }
+
+
+
+
+    public DateTime ReleaseDate { get; set; }
+
+
+    public required string ImageUrl { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,135 @@
+using Gamestore.Api.Entities;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        const string GetGameEndpointName = "GetGame";
+
+
+        List<Game> games = new(){
+ new Game(){
+        Id = 1,
+        Name = "Street fighter",
+
+
+        Genre = "Fighting",
+
+
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUrl ="https://placeholder.co/100:"
+    },
+ new Game(){
+        Id = 2,
+        Name = "Teken3",
+
+
+        Genre = "Fighting",
+
+
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUrl ="https://placeholder.co/100:"
+    },
+    new Game(){
+        Id = 3,
+        Name = "Mario Brothers",
+
+
+        Genre = "Fighting",
+
+
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUrl ="https://placeholder.co/100:"
+    },
+     new Game(){
+        Id = 4,
+        Name = "GTA 7",
+
+
+        Genre = "Fighting",
+
+
+        Price = 19.99M,
+        ReleaseDate = new DateTime(1991,2,1),
+        ImageUrl ="https://placeholder.co/100:"
+    },
+};
+
+
+        var builder = WebApplication.CreateBuilder(args);
+        var app = builder.Build();
+
+        app.MapGet("/games", () => games);
+
+        app.MapGet("/games/{id}", (int id) =>
+        {
+            Game? game = games.Find(game => game.Id == id);
+
+            if (game is null)
+            {
+
+                return Results.NotFound();
+            }
+
+
+            return Results.Ok(game);
+
+
+        })
+        .WithName("GetGame");
+
+
+        app.MapPost("/games", (Game game) =>
+        {
+            game.Id = games.Max(game => game.Id) + 1;
+            games.Add(game);
+
+
+            return Results.CreatedAtRoute(GetGameEndpointName, new { id = game.Id }, game);
+
+        });
+
+        app.MapPut("/games/{id}", (int id, Game updatedGame) =>
+        {
+            Game? existingGame = games.Find(game => game.Id == id);
+
+            if (existingGame is null)
+            {
+
+                return Results.NotFound();
+            }
+
+            existingGame.Name = updatedGame.Name;
+            existingGame.Genre = updatedGame.Genre;
+            existingGame.Price = updatedGame.Price;
+            existingGame.ReleaseDate = updatedGame.ReleaseDate;
+            existingGame.ImageUrl = updatedGame.ImageUrl;
+
+            return Results.NoContent();
+
+        });
+
+
+        app.MapDelete("/games/{id}", (int id) =>
+        {
+
+
+            Game? game = games.Find(game => game.Id == id);
+
+            if (game is not null)
+            {
+
+                games.Remove(game);
+            }
+
+
+
+            return Results.NoContent();
+        });
+
+        app.Run();
+    }
+}
